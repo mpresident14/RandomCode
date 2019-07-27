ec65283c41c6b265088d5ff43790f3ec0d8dc866
mpresident14 <mcprez10@gmail.com>
2019-07-27 11:44:51 -0700

    Restructure packages

--- java/run_java.py
+++ java/run_java.py
@@ -1,19 +1,40 @@
 import os
 import sys
+import argparse
 
 def help():
   return ("Run this from your top level directory. Your .java files " 
-  "should be in src/ and there should be a bin/ folder for output .class files.\n"
-  "Syntax: python run_java.py path.to.my.class.ClassName")
+  "should be in src/ and there should be a bin/ folder for output .class files. " 
+  "The CLASSPATH env variable should have src/, bin/, test/, and "
+  "C:\Program Files (x86)\junit4.10\junit-4.10.jar.\n"
+  "[Syntax: python run_java.py [-t] path.to.my.class.ClassName]")
+
+def run(pathDots, pathSlashes):
+  compileCmd = f"javac src/{pathSlashes}.java -d bin"
+  runCmd = f"java {pathDots}"
+
+  return compileCmd, runCmd
+
+def test(pathDots, pathSlashes):
+  compileCmd = f"javac test/{pathSlashes}.java -d bin"
+  runCmd = f"java org.junit.runner.JUnitCore {pathDots}"
+
+  return compileCmd, runCmd
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-      print(help())
-      exit()
-    pathDots = sys.argv[1]
+    # if len(sys.argv) != 2:
+    #   print(help())
+    #   exit()
+    parser = argparse.ArgumentParser(description='Run java files.')
+    parser.add_argument("-t", action='store_true')
+    parser.add_argument("className")
+    args = vars(parser.parse_args())
+
+    pathDots = args["className"]
     pathSlashes = "/".join(pathDots.split("."))
-    compileCmd = f"javac -classpath src/ src/{pathSlashes}.java -d bin"
-    runCmd = f"java -cp bin/ {pathDots}"
+
+    compileCmd, runCmd = \
+        test(pathDots, pathSlashes) if args["t"] else run(pathDots, pathSlashes)
 
     os.system(compileCmd)
     os.system(runCmd)
---
+++ java/test/hashmap/MapOpenAddressingTest.java
@@ -0,0 +1,14 @@
+package hashmap;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MapOpenAddressingTest {
+
+  public MapOpenAddressingTest() {}
+
+  @Test
+  public void fcn() {
+    assertEquals(3, 2 + 1);
+  }
+}
