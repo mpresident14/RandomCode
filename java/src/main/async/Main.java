64ef086207bdc35a9e263de1e8399648c47527b9
mpresident14 <mcprez10@gmail.com>
2019-08-26 03:29:13 -0700

    Update async Main class with demo

--- java/src/main/async/Main.java
+++ java/src/main/async/Main.java
@@ -1,46 +1,55 @@
 package async;
 
+import java.util.Random;
+
 public class Main {
-  public static int longRunningOp(int n) {
+  private static Random random = new Random();
+  private static int longRunningOp(int n) {
     try {
-      Thread.sleep(1000);
+      Thread.sleep(random.nextInt(3000));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
     return n;
   }
+  private static long getThreadId() {
+    return Thread.currentThread().getId();
+  }
+
+  private static AsyncOperation<String> getAsyncOp(int index) {
+    return AsyncGraph
+        .createAsync(() -> longRunningOp(index))
+        .peek(num -> System.out.println(String.format("Thread %d: long op returned %d", getThreadId(), num)))
+        .then(num -> Integer.toString(num));
+  }
 
   public static void main(String[] args) {
-    AsyncOperation<String> asyncOp1 = 
+    AsyncOperation<String> asyncOp1 = getAsyncOp(1);
+    AsyncOperation<String> asyncOp2 = getAsyncOp(2);
+    AsyncOperation<String> asyncOp3 = 
         AsyncGraph
-            .createAsync(() -> longRunningOp(8))
-            .peek(num -> System.out.println("long op finished"))
-            .peek(num -> System.out.println("long op returned " + num))
-            .then(num -> num + 0.5)
-            .then(dec -> Double.toString(dec).substring(1));
-
-    AsyncOperation<String> asyncOp2 = 
+            .createImmediateAsync(3)
+            .thenCollapse(num -> getAsyncOp(num));
+    AsyncOperation<Integer> asyncOp4 = 
         AsyncGraph
-            .createImmediateAsync(8)
-            .then(num -> longRunningOp(num + 7))
-            .peek(num -> System.out.println("Thread: id " + Thread.currentThread().getId())            )
-            .then(num -> Double.toString(num).substring(1));
+            .createImmediateAsync(4)
+            .thenConsume(num -> {})
+            .peek(v -> System.out.println(String.format("Thread %d: Value is %s", getThreadId(), v)))
+            .then(v -> 4);
 
-    AsyncGraph.runAsync(asyncOp1, 
-        str -> System.out.println(
-            String.format("Thread %d computed %s", Thread.currentThread().getId(), str)));
+    AsyncOperation<String> asyncOpFinal = 
+        AsyncGraph
+            .combineAsync(
+                asyncOp1,
+                asyncOp2,
+                asyncOp3,
+                asyncOp4,
+                (str1, str2, str3, int4) -> String.join(", ", str1, str2, str3, Integer.toString(int4)));
+
+    AsyncGraph.runAsync(asyncOpFinal, str -> System.out.println("Final result is " + str));
 
     for (int i = 0; i < 5; i++) {
       System.out.println("Main thread: id " + Thread.currentThread().getId());
     }
-
-    System.out.println(
-        String.format("Thread %d computed %s", 
-            Thread.currentThread().getId(), AsyncGraph.getSync(asyncOp2)));
-
-    AsyncGraph.runAsync(
-        asyncOp2, 
-        str -> System.out.println(
-            String.format("Thread %d computed %s", Thread.currentThread().getId(), str)));     
   }
 }
