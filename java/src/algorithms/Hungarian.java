83d430ef3dd62bec61b07ab483096bbd571f2712
mpresident14 <mcprez10@gmail.com>
2020-05-24 04:33:49 -0700

    Hungarian method complete.

--- java/src/algorithms/Hungarian.java
+++ java/src/algorithms/Hungarian.java
@@ -1,23 +1,113 @@
 package algorithms;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.Random;
 import java.util.Set;
-import java.util.Stack;
 import java.util.stream.IntStream;
-
-import other.Pair;
 
 public class Hungarian {
 
+  private static class AltTree {
+    private class Node {
+      int vertex;
+      boolean isXVertex;
+      Set<Node> children;
+      Node parent;
+
+      Node(int vertex, boolean isXVertex, Node parent) {
+        this.vertex = vertex;
+        this.isXVertex = isXVertex;
+        this.children = new HashSet<>();
+        this.parent = parent;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (!(obj instanceof Node)) {
+          return false;
+        }
+
+        Node node = (Node) obj;
+        return vertex == node.vertex && isXVertex == node.isXVertex;
+      }
+
+      @Override
+      public int hashCode() {
+        return vertex;
+      }
+    }
+
+    Node root;
+
+    AltTree(int vertex) {
+      this.root = new Node(vertex, true, null);
+    }
+
+    /* Adds y as a child of prevX and returns the augmenting path from the root (i.e., u) to y */
+    List<Integer> getPath(int prevX, int y) {
+      Queue<Node> q = new LinkedList<>();
+      q.add(root);
+      while (!q.isEmpty()) {
+        Node node = q.poll();
+        // Found prevX, backtrack through the tree to find the path
+        if (node.isXVertex && node.vertex == prevX) {
+          List<Integer> augPath = new LinkedList<>();
+          augPath.add(0, y);
+          Node currentNode = node;
+          while (currentNode != null) {
+            augPath.add(0, currentNode.vertex);
+            currentNode = currentNode.parent;
+          }
+          return augPath;
+        }
+
+        // Continue searching
+        node.children.forEach(q::add);
+      }
+
+      // Won't get here
+      throw new NoSuchElementException(prevX + " was not found");
+    }
+
+    /* Adds y as a child of prevX and x as a child of y */
+    void addToTree(int prevX, int y, int x) {
+      // This is a tree, so no visited set required
+      Queue<Node> q = new LinkedList<>();
+      q.add(root);
+      while (!q.isEmpty()) {
+        Node node = q.poll();
+        // Found prevX
+        if (node.isXVertex && node.vertex == prevX) {
+          Node yNode = new Node(y, false, node);
+          yNode.children.add(new Node(x, true, yNode));
+          node.children.add(yNode);
+          return;
+        }
+
+        // Continue searching
+        node.children.forEach(q::add);
+      }
+    }
+  }
+
   private static int FREE = -1;
 
-  public static int[] hungarian(int[][] weights) {
+  /* To get the minimum cost, negate the weights and find the maximum */
+  public static int[] hungarianMin(int[][] weights) {
+    int[][] negWeights = new int[weights.length][];
+    for (int i = 0; i < negWeights.length; ++i) {
+      negWeights[i] = Arrays.stream(weights[i]).map(w -> -w).toArray();
+    }
+    return hungarianMax(negWeights);
+  }
+
+  public static int[] hungarianMax(int[][] weights) {
     int numJobs = weights.length;
 
     // Initialize empty matching
@@ -38,104 +128,80 @@ public class Hungarian {
     while (numMatches != numJobs) {
       Set<Integer> s = new HashSet<>();
       // S = {u}
-      int u = -1;
-      for (int xi = 0; xi < numJobs; ++xi) {
-        if (xMatching[xi] == FREE) {
-          u = xi;
-          s.add(xi);
-          break;
-        }
-      }
-      // T = {}
+      int u =
+          IntStream.range(0, numJobs).filter(xi -> xMatching[xi] == FREE).findFirst().getAsInt();
+      s.add(u);
       Set<Integer> t = new HashSet<>();
+
+      int uLabel = xLabels[u];
+      int[] uWeights = weights[u];
 
       // Initialize alphas for each y in Y
       int[] alphas = new int[numJobs];
-      for (int yi = 0; yi < numJobs; ++yi) {
-        alphas[yi] = xLabels[u] + yLabels[yi] - weights[u][yi];
-      }
+      Arrays.setAll(alphas, yi -> uLabel + yLabels[yi] - uWeights[yi]);
 
       // Calculate initial N_L(S)
       BitSet neighborhoodS = new BitSet(numJobs);
-      int uLabel = xLabels[u];
-      int[] uWeights = weights[u];
       for (int yi = 0; yi < numJobs; ++yi) {
         if (uLabel + yLabels[yi] == uWeights[yi]) {
           neighborhoodS.set(yi);
         }
       }
 
-      // Initialize augmenting path
-      List<Integer> augPath = new ArrayList<>();
-      augPath.add(u);
+      // Initialize alternating tree
+      AltTree altTree = new AltTree(u);
 
       while (true) {
         // N_L(S) == T
         if (t.size() == neighborhoodS.cardinality()
-          && t.stream().allMatch(vertex -> neighborhoodS.get(vertex))) {
+            && t.stream().allMatch(vertex -> neighborhoodS.get(vertex))) {
           int minAlpha =
-              IntStream
-                  .range(0, numJobs)
+              IntStream.range(0, numJobs)
                   .filter(yi -> !t.contains(yi))
                   .map(yi -> alphas[yi])
                   .min()
                   .getAsInt();
 
-          for (int x : s) {
-            xLabels[x] -= minAlpha;
-          }
-          for (int y : t) {
-            yLabels[y] += minAlpha;
-          }
+          s.forEach(x -> xLabels[x] -= minAlpha);
+          t.forEach(y -> yLabels[y] += minAlpha);
 
           // Add all edges with minAlpha to the neighborhood of S
+          // Since every x in S decreased by minAlpha, we need to update alphas
           for (int yi = 0; yi < numJobs; ++yi) {
             if (alphas[yi] == minAlpha) {
               neighborhoodS.set(yi);
             }
-          }
-
-          // Since every x in S decreased by minAlpha, we need to update alphas
-          for (int i = 0; i < numJobs; ++i) {
-            alphas[i] -= minAlpha;
+            alphas[yi] -= minAlpha;
           }
         }
 
         // N_L(S) != T, find y in N_L(S)\T
-        int y = -1;
-        for (int yi = 0; yi < numJobs; ++yi) {
-          if (neighborhoodS.get(yi) && !t.contains(yi)) {
-            y = yi;
-            break;
-          }
-        }
+        int y =
+            IntStream.range(0, numJobs)
+                .filter(yi -> neighborhoodS.get(yi) && !t.contains(yi))
+                .findFirst()
+                .getAsInt();
 
-        // Find the x that gave minAlpha (TODO: should store this)
-        int prevX = -1;
-        for (int x : s) {
-          if (alphas[y] == xLabels[x] + yLabels[y] - weights[x][y]) {
-            prevX = x;
-          }
-        }
+        // Find an x in S that was adjacent to y
+        int yLabel = yLabels[y];
+        int prevX = s.stream().filter(x -> xLabels[x] + yLabel == weights[x][y]).findFirst().get();
 
         int x = yMatching[y];
         if (x == FREE) {
-          // y is free
-          backtrackAugPath(augPath, prevX);
-          augPath.add(y);
+          List<Integer> augPath = altTree.getPath(prevX, y);
           int numEdges = augPath.size() - 1;
           // Unmatch matched edges
           for (int i = 1; i < numEdges; i += 2) {
             // Odd edges are matched (start with y in Y), so unmatch them
             int yi = augPath.get(i);
-            int xi = augPath.get(i+1);
+            int xi = augPath.get(i + 1);
             yMatching[yi] = FREE;
             xMatching[xi] = FREE;
           }
           for (int i = 0; i < numEdges; i += 2) {
-              // Even edges are unmatched (start with x in X), so match them
+            // Even edges are unmatched (start with x in X), so match them
             int xi = augPath.get(i);
-            int yi = augPath.get(i+1);
+            int yi = augPath.get(i + 1);
             xMatching[xi] = yi;
             yMatching[yi] = xi;
           }
@@ -156,40 +222,33 @@ public class Hungarian {
 
           // Update alphas
           for (int yi = 0; yi < numJobs; ++yi) {
-            alphas[yi] = Math.min(alphas[yi], xLabels[x] + yLabels[yi] - weights[x][yi]);
+            alphas[yi] = Math.min(alphas[yi], xLabel + yLabels[yi] - xWeights[yi]);
           }
 
-          backtrackAugPath(augPath, prevX);
-          augPath.add(y);
-          augPath.add(x);
+          altTree.addToTree(prevX, y, x);
         }
       }
-
-
     }
 
     return xMatching;
   }
 
-
-  /* Remove vertices from the end of the augmenting path until we reach prevX */
-  private static void backtrackAugPath(List<Integer> augPath, int prevX) {
-    ListIterator<Integer> iter = augPath.listIterator(augPath.size());
-    while (iter.previous() != prevX) {
-      // remove x_i
-      iter.remove();
-      iter.previous();
-      // remove y_i
-      iter.remove();
-    }
-  }
-
-
   public static void main(String[] args) {
-    int[][] weights = {
-        {108,125,180},
-        {150,135,175},
-        {122,148,250}};
-    System.out.println(Arrays.toString(hungarian(weights)));
+    Random random = new Random();
+    int size = 1000;
+    int[][] weights = new int[size][size];
+    for (int j = 0; j < weights.length; ++j) {
+      weights[j] = new int[size];
+      Arrays.setAll(weights[j], n -> random.nextInt());
+    }
+
+    long startTime = System.nanoTime();
+    Hungarian.hungarianMax(weights);
+    long endTime = System.nanoTime();
+
+    double durationMs = (endTime - startTime) / 1000000.0;
+    System.out.println(durationMs);
+
+    Hungarian.hungarianMax(weights);
   }
 }
